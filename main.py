7ccc176373a739d052a185b8a3031733af36ce6d
OrionCoderMeta <s307167@studenti.polito.it>
2025-03-04 17:38:12 +0100

    Svolto l'esercizio non toccando dictionary.py perchè mi sembrava inutile

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
 import translator as tr
 
 t = tr.Translator()
+check = True
+t.loadDictionary("dictionary.txt")
 
-
-while(True):
+while check:
 
     t.printMenu()
+    txtIn = int(input("Che cosa desideri fare?: "))
 
-    t.loadDictionary("filename.txt")
-
-    txtIn = input()
-
-    # Add input control here!
-
+    #Aggiungiamo parola
     if int(txtIn) == 1:
-        print()
-        txtIn = input()
-        pass
-    if int(txtIn) == 2:
-        pass
-    if int(txtIn) == 3:
-        pass
-    if int(txtIn) == 4:
+        parola = input("Perfavore aggiungi la parola nel formato:\n"
+                       "Parola da tradurre [SPAZIO] traduzione: ")
+        t.handleAdd(parola)
+    #Cerchiamo una parola
+    elif int(txtIn) == 2:
+        parola = input("Scrivi la parola da cercare: ")
+        t.handleTranslate(parola)
+    #Cerchiamo con wildCard
+    elif int(txtIn) == 3:
+        parola = input("Scrivi la parola da cercare: ")
+        t.handleWildCard(parola)
+    elif int(txtIn) == 4:
+        print("Grazie del tuo aiuto!")
         break
+    else:
+        print("Mi dispiace ma inserisci un numero da 1 a 4")
+
+    #Chiedere se vuole continuare
+    now = input("Desideri continuare?: ")
+    if now == "no":
+        print("Grazie del tuo aiuto!")
+        check = False
--- translator.py
+++ translator.py
@@ -1,27 +1,55 @@
+
 class Translator:
 
     def __init__(self):
-        pass
+        self.diz = {}
 
     def printMenu(self):
-        # 1. Aggiungi nuova parola
-        # 2. Cerca una traduzione
-        # 3. Cerca con wildcard
-        # 4. Exit
-        pass
+        print(f"1. Aggiungi nuova parola \n2. Cerca una parola \n3. Cerca con wildcard \n4. Exit")
 
     def loadDictionary(self, dict):
-        # dict is a string with the filename of the dictionary
-        pass
+        with open(dict) as file:
+            for line in file:
+                line = line.strip().split(" ")
+                if line[0] not in self.diz:
+                    self.diz[line[0]] = [line[1]]
+                else:
+                    self.diz[line[0]].append(line[1])
 
     def handleAdd(self, entry):
-        # entry is a tuple <parola_aliena> <traduzione1 traduzione2 ...>
-        pass
+        entry = entry.strip()
+        entry = entry.split(" ")
+        if entry[0] not in self.diz:
+            self.diz[entry[0]] = [entry[1]]
+            with open("dictionary.txt", "a") as file:
+                file.write(f"{entry[0]} {entry[1]} \n")
+            print("Grazie per la nuova parola!")
+        elif entry[0] in self.diz and entry[1] not in self.diz[entry[0]]:
+            self.diz[entry[0]].append(entry[1])
+            with open("dictionary.txt", "a") as file:
+                file.write(f"{entry[0]} {entry[1]} \n")
+            print(f"Grazie per la nuova traduzione di {entry[0]} !")
+        else:
+            print("Grazie ma la tua traduzione già esiste nel sistema!")
 
     def handleTranslate(self, query):
-        # query is a string <parola_aliena>
-        pass
+        if query in self.diz:
+            r = ""
+            for el in self.diz[query]:
+                r += (el + " ")
+            print(f"Ecco la/le traduzioni: {r}")
+        else:
+            print("Mi dispiace ma la parola non ha traduzione :(")
 
-    def handleWildCard(self,query):
-        # query is a string with a ? --> <par?la_aliena>
-        pass
+    def handleWildCard(self, query):
+        parola = query.replace("?", "")  # Rimuove il punto interrogativo
+        traduzioni = []
+
+        for word in self.diz:
+            if parola in word:
+                traduzioni.append(f"{word} : {', '.join(self.diz[word])}")
+
+        if traduzioni:
+            print(f"Le traduzioni per la parola '{query}' sono:\n" + "\n".join(traduzioni))
+        else:
+            print(f"Nessuna traduzione trovata per '{query}'")
